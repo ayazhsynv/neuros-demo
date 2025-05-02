532d72c13348997fee8bc906ff6d562bc8cda1e1
ayazhsynv <ayazhuseynov@proton.me>
2025-05-02 04:05:31 +0400

    finished layout, header, foooter

--- client/src/components/layout/UiHeader.jsx
+++ client/src/components/layout/UiHeader.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./UiHeader.module.css";
 
 const UiHeader = () => {
   return (
@@ -21,30 +22,200 @@ const UiHeader = () => {
         </div>
         <div className="flex-4/6">
           <ul className="flex items-center justify-center gap-4.25 font-bold">
-            <li className="text-ui-black bg-ui-white rounded-4xl px-4.5 py-2">
-              <Link to="/">
+            <li className={styles.navhome}>
+              <Link
+                className=" text-ui-black bg-ui-white rounded-4xl px-4.5 py-2"
+                to="/"
+              >
                 Home <i className=" ri-arrow-drop-down-fill"></i>
               </Link>
+              <div className={styles.navhome_dropdown}>
+                <ul className="flex flex-col gap-5 w-42.5 ">
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Artificial Intelligence</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Neural Networks</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">AI Agency</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Image Generator</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Voice Generator</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Chatbot</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">AI Solutions</Link>
+                  </li>
+                </ul>
+                <ul className="flex flex-col gap-5 w-42.5 ">
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">AI Marketing</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">AI Startup</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Vision</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">AI Consulting</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Video Voiceovover</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">AI Software / SaaS</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Science Lab</Link>
+                  </li>
+                </ul>
+                <ul className="flex flex-col gap-5 w-42.5 ">
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Creative Bureau</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">IT Services</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Business</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Gadget</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Application</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Light Version</Link>
+                  </li>
+                </ul>
+                <div className="flex flex-col w-49  overflow-hidden">
+                  <img
+                    className=" w-full rounded-md object-cover "
+                    src="src/assets/home-nav-light.png"
+                    alt=""
+                  />
+                  <p className=" text-md font-bold">Light Version</p>
+                </div>
+              </div>
             </li>
-            <li className="px-4.5 py-2 hover:bg-ui-orange transition duration-300 rounded-4xl">
-              <Link to="#">
+            <li className={styles.navhome}>
+              <Link
+                className="px-4.5 py-2 hover:bg-ui-orange transition duration-300 rounded-4xl"
+                to="#"
+              >
                 Pages <i className=" ri-arrow-drop-down-fill"></i>
               </Link>
+              <div className={styles.navhome_dropdown}>
+                <ul className="flex flex-col gap-5 w-42.5 ">
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">About</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Team</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Projects</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Careers</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Case Studies</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Pricing Plans</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Gallery Grid</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Gallery Masonry</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">FAQ</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Typography</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="/404">404 Page</Link>
+                  </li>
+                </ul>
+              </div>
             </li>
-            <li className="px-4.5 py-2  hover:bg-ui-orange transition duration-300 rounded-4xl">
-              <Link to="#">
+            <li className={styles.navhome}>
+              <Link
+                className="px-4.5 py-2  hover:bg-ui-orange transition duration-300 rounded-4xl"
+                to="#"
+              >
                 Services <i className=" ri-arrow-drop-down-fill"></i>
               </Link>
+              <div className={styles.navhome_dropdown}>
+                <ul className="flex flex-col gap-5 w-42.5 ">
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Services Page</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Service Single</Link>
+                  </li>
+                </ul>
+              </div>
             </li>
-            <li className="px-4.5 py-2  hover:bg-ui-orange transition duration-300 rounded-4xl">
-              <Link to="#">
+            <li className={styles.navhome}>
+              <Link
+                className="px-4.5 py-2  hover:bg-ui-orange transition duration-300 rounded-4xl"
+                to="#"
+              >
                 Shop <i className=" ri-arrow-drop-down-fill"></i>
               </Link>
+              <div className={styles.navhome_dropdown}>
+                <ul className="flex flex-col gap-5 w-42.5 ">
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Products List</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Product Single</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Cart</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Checkout</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Account</Link>
+                  </li>
+                </ul>
+              </div>
             </li>
-            <li className="px-4.5 py-2  hover:bg-ui-orange transition duration-300 rounded-4xl">
-              <Link to="/blog">
+            <li className={styles.navhome}>
+              <Link
+                className="px-4.5 py-2  hover:bg-ui-orange transition duration-300 rounded-4xl"
+                to="/blog"
+              >
                 Blog <i className=" ri-arrow-drop-down-fill"></i>
               </Link>
+              <div className={styles.navhome_dropdown}>
+                <ul className="flex flex-col gap-5 w-42.5 ">
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Blog Classic</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Blog Grid</Link>
+                  </li>
+                  <li className="hover:text-ui-orange transition duration-300">
+                    <Link to="#">Blog Single</Link>
+                  </li>
+                </ul>
+              </div>
             </li>
             <li className="px-4.5 py-2  hover:bg-ui-orange transition duration-300 rounded-4xl">
               <Link to="#">Contacts</Link>
