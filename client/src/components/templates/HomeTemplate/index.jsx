024fff513921cf75d80f1b19afc62f7e8d1526c6
ayazhsynv <ayazhuseynov@proton.me>
2025-05-04 03:31:34 +0400

    deleted server, will reinstall

--- client/src/components/templates/HomeTemplate/index.jsx
+++ client/src/components/templates/HomeTemplate/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HomeHero from "../../sections/HomeHero";
 import HomeAbout from "../../sections/HomeAbout";
+import HomeService from "../../sections/HomeServices";
 
 const HomeTemplate = () => {
   return (
     <div>
       <HomeHero />
       <HomeAbout />
+      <HomeService />
     </div>
   );
 };
